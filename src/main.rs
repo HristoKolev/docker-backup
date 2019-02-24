d8335169d3517a672399cdc7133d3417764b3ec0
Hristo Kolev <hristo.a.kolev@gmail.com>
2019-02-24 18:22:36 +0200

    updated async packages

--- src/main.rs
+++ src/main.rs
@@ -25,9 +25,9 @@ async fn main_async () {
 
     let container_details = await!(get_connected_containers(&docker, &volume));
 
-    
-
     println!("{:#?}", container_details);
+
+
 }
 
 fn get_volume_name() -> String {
@@ -42,7 +42,6 @@ fn get_volume_name() -> String {
 fn create_client () -> Docker {
 
     let uri = "http://dev-host.lan:2376".parse().unwrap();
-
     let docker = Docker::host(uri);
 
     docker
@@ -50,9 +49,8 @@ fn create_client () -> Docker {
 
 async fn get_volume_by_name<'a> (docker: &'a Docker, volume_name: &'a str) -> VolumeRep {
 
-    let volume_mountpoints : Vec<_> = await!(
-        docker.volumes().list()
-    ).expect("There was an error while listing the volumes.");
+    let volume_mountpoints : Vec<_> = await!(docker.volumes().list())
+        .expect("There was an error while listing the volumes.");
 
     let volume: VolumeRep = volume_mountpoints
         .into_iter()
@@ -65,24 +63,19 @@ async fn get_volume_by_name<'a> (docker: &'a Docker, volume_name: &'a str) -> Vo
 
 async fn get_connected_containers<'a>(docker: &'a Docker, volume: &'a VolumeRep) -> Vec<ContainerDetailsRep> {
 
-    let container_reps: Vec<ContainerRep> = await!(
-        docker.containers()
-            .list(&Default::default())
-    ).expect("There was an error while listing the containers.");
+    let container_reps: Vec<ContainerRep> = await!(docker.containers().list(&Default::default()))
+        .expect("There was an error while listing the containers.");
 
     let mut connected_containers = Vec::new();
 
     for container_rep in container_reps {
 
         let container = docker.containers().get(&container_rep.id);
-
         let container_detail = await!(container.inspect())
             .expect(&format!("There was an error while getting details for container `{}`.", container_rep.id));
 
         for mount in &container_detail.mounts {
-
             if mount.source == volume.mountpoint {
-
                 connected_containers.push(container_detail.clone());
             }
         }
