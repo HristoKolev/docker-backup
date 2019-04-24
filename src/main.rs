2b43f355d9fd95bf417a99462915d2c4eae8d85b
Hristo Kolev <hristo.a.kolev@gmail.com>
2019-04-24 19:51:57 +0300

    getting the results

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use std::process::{Command, Stdio};
-use std::io::{BufReader, BufRead, BufWriter, Write};
+use std::io::{BufReader, BufRead, Write};
 use std::thread;
 
 fn main() {
 
     let res = exec("echo 123");
+
+    println!("{:#?}", res);
 }
 
 fn exec(command: &str) -> CommandResult {
@@ -16,55 +18,53 @@ fn exec(command: &str) -> CommandResult {
         .spawn()
         .expect("failed to spawn a process.");
 
-    let mut stdout = process.stdout.take().unwrap();
-    let mut stderr = process.stderr.take().unwrap();
-    let mut stdin = process.stdin.as_mut().unwrap();
-
-    // let mut stdout_result = String::new();
-    // let mut stderr_result= String::new();
+    let stdout = process.stdout.take().unwrap();
+    let stderr = process.stderr.take().unwrap();
+    let stdin = process.stdin.as_mut().unwrap();
 
     let std_out_thread= thread::spawn(move || {
 
-        let stdout_buff = BufReader::new(stdout);
+        let buff = BufReader::new(stdout);
 
-        // let mut std_out_vec = String::new();
+        let mut result = String::new();
 
-        for line in stdout_buff.lines() {
-            let formatted = format!("OUT | {}", line.unwrap());
-            /// std_out_vec.push_str(&formatted);
+        for line in buff.lines() {
+            let formatted = format!("OUT | {}\n", line.unwrap());
+            result.push_str(&formatted);
             write_to_out(&formatted);
         }
 
-        // stdout_result = std_out_vec;
+        result
     });
 
     let std_err_thread= thread::spawn(move || {
 
-        let stderr_buff = BufReader::new(stderr);
+        let buff = BufReader::new(stderr);
 
-        // let mut std_err_vec = String::new();
+        let mut result = String::new();
 
-        for line in stderr_buff.lines() {
-            let formatted = format!("ERR | {}", line.unwrap());
-            //  std_err_vec.push_str(&formatted);
+        for line in buff.lines() {
+            let formatted = format!("ERR | {}\n", line.unwrap());
+            result.push_str(&formatted);
             write_to_err(&formatted);
         }
 
-        // stderr_result = std_err_vec;
+        result
     });
 
+    stdin.write_all("set -exu\n".as_bytes()).unwrap();
     stdin.write_all(format!("{}\n", command).as_bytes()).unwrap();
     stdin.write_all("exit $?;\n".as_bytes()).unwrap();
 
-    std_out_thread.join().unwrap();
-    std_err_thread.join().unwrap();
+    let out_result = std_out_thread.join().unwrap();
+    let err_result = std_err_thread.join().unwrap();
 
     let exit_status = process.wait().unwrap();
 
     return CommandResult {
         status: exit_status.code().unwrap(),
-        // stdout: stdout_result,
-        // stderr: stderr_result,
+        stdout: out_result,
+        stderr: err_result,
     };
 }
 
@@ -79,6 +79,6 @@ fn write_to_err(text: &str) {
 #[derive(Debug)]
 struct CommandResult {
     status: i32,
-    // stdout: String,
-    // stderr: String,
+    stdout: String,
+    stderr: String,
 }
