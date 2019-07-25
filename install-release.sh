d2d928d9f5a16653cc4648455fb434832ac2845f
Hristo Kolev <hristo.a.kolev@gmail.com>
2019-07-25 23:33:50 +0300

    build script

--- install-release.sh
+++ install-release.sh
@@ -2,5 +2,5 @@
 
 set -exu
 
-rsync -aP ./target/release/xdxd-backup root@docker-vm1.lan:/root/xdxd-backup/xdxd-backup
-rsync -aP ./target/release/xdxd-backup /home/hristo/xdxd-backup/xdxd-backup
+# rsync -aP ./target/x86_64-unknown-linux-musl/release/xdxd-backup root@docker-vm1.lan:/root/xdxd-backup/xdxd-backup
+rsync -aP ./target/x86_64-unknown-linux-musl/release/xdxd-backup /home/hristo/xdxd-backup/xdxd-backup
